50eacab0926fff149d2dd9b44325c0fdf12cc294
Steph Johns <steph.johns@mrgsystems.co.uk>
2025-06-02 17:32:30 +0100

    comments removed

--- src/app/subject-detail/subject-detail.component.ts
+++ src/app/subject-detail/subject-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CommonModule } from '@angular/common'; // Add this
-import { RouterModule } from '@angular/router'; // If you're using routing
+import { CommonModule } from '@angular/common'; 
+import { RouterModule } from '@angular/router';
 
 @Component({  
   selector: 'app-subject-detail',
@@ -15,12 +15,10 @@ export class SubjectDetailComponent implements OnInit {
   subject: any;
 
   ngOnInit() {
-    // Get the subject from router state
     this.subject = history.state.subject;
     
     if (!this.subject) {
       console.error('Subject not found!');
-      // Optionally handle the error here (e.g. redirect or show an error message)
     }
     console.log('Loaded subject:', this.subject);
   }
