f382a9bb7e634ac9e983ec888f99f7447f7563cc
Steph Johns <steph.johns@mrgsystems.co.uk>
2025-06-20 19:29:03 +0100

    navigation added to most buttons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
 
   ngAfterViewInit(): void {
     const burger = document.getElementById('burgerMenu');
@@ -19,5 +21,38 @@ export class HeaderComponent {
       });
     }
   }
+
+    goToServices() {
+    this.router.navigate(['']).then(() => {
+      const el = document.getElementById('services');
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
+
+goHome() {
+  if (this.router.url === '/') {
+    const el = document.getElementById('top');
+    if (el) el.scrollIntoView({ behavior: 'smooth' });
+  } else {
+    this.router.navigate(['/']).then(() => {
+      const el = document.getElementById('top');
+      if (el) el.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+}
+
+goToAbout() {
+  if (this.router.url === '/') {
+    const el = document.getElementById('about');
+    if (el) el.scrollIntoView({ behavior: 'smooth' });
+  } else {
+    this.router.navigate(['/']).then(() => {
+      const el = document.getElementById('about');
+      if (el) el.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+}
 
 }
